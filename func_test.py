202b127ee330a56b0fa9cc4da1378f36b1fb08ad
Prabhjyot Dhanoa <prabhjyotdhanoa@gmail.com>
2023-05-22 19:40:38 -0400

    Made Test system, Card Class, and Main file

---
+++ func_test.py
@@ -0,0 +1,5 @@
+import card
+
+def function_test():
+    test_card = card.Card("Test Creature","Creature")
+    print(test_card)
---
+++ main.py
@@ -0,0 +1,4 @@
+import func_test
+import card
+
+func_test.function_test()
